04cdc8105f1ddea11fadb8b3aecbadcfe5465cb8
burn-c <chopcsom@gmail.com>
2020-01-14 21:48:27 -0300

    Feacture: RegisterDev

---
+++ backend/src/routes.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const DeVController = require('./controllers/DevController');
+
+const routes = Router();
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+// a - Query Params: req.params ( Filtros, ordenação, paginação...)
+// b - Route Params: req.params (Identificar um recurso na alteração ou remoção)
+// c - Body: req.body ( Dados para criação ou alteração de um registro )
+
+//
+
+routes.get('/devs', DeVController.index);
+routes.post('/devs', DeVController.store);
+
+module.exports = routes;
