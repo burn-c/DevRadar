66482b3ddc4449999b98320ac0e66ac5919f4adc
burn-c <chopcsom@gmail.com>
2020-01-26 15:38:35 -0300

    Feacture: WebApp

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
 // Propriedade: Informações que um componente PAI passa para um componente FILHO
@@ -11,88 +15,42 @@ import './Main.css';
 
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+ 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  },[])
+   
+  async function handleAddDev(data) {
+
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data])
+    
+  }
+
   return (
   <div id="app">
     <aside>
       <strong>Cadastrar</strong>
-      <form>
-        <div className="input-block">
-          <label htmlFor="github_username">Usuário do Github</label>
-          <input name="github_username" id="github_username" required />
-        </div>
-
-
-        <div className="input-block">
-          <label htmlFor="techs">Tecnologias</label>
-          <input name="techs" id="techs" required />
-        </div>
-
-        <div className="input-group">
-
-
-        <div className="input-block">
-          <label htmlFor="latitude">Latitude</label>
-          <input name="latitude" id="latitude" required />
-        </div>
-
-
-        <div className="input-block">
-          <label htmlFor="Longitude">Longitude</label>
-          <input name="Longitude" id="Longitude" required />
-        </div>
-
-        </div>
-
-        <button type="submit" >Salvar</button>
-      </form>
+      <DevForm onSubmit={handleAddDev} />
     </aside>
     
     <main>
       <ul>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars0.githubusercontent.com/u/54965836?s=460&v=4" alt="FotoPerfil"/>
-            <div className="user-info">
-              <strong>Carlos Oliveira</strong>
-              <span>ReactJS, React Native, Node.JS</span>
-            </div>
-          </header>
-          <p>Aqui tem uma historia muito linda, pode ter certeza! Combinado?</p>
-          <a href="https://github.com/burn-c">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars0.githubusercontent.com/u/54965836?s=460&v=4" alt="FotoPerfil"/>
-            <div className="user-info">
-              <strong>Carlos Oliveira</strong>
-              <span>ReactJS, React Native, Node.JS</span>
-            </div>
-          </header>
-          <p>Aqui tem uma historia muito linda, pode ter certeza! Combinado?</p>
-          <a href="https://github.com/burn-c">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars0.githubusercontent.com/u/54965836?s=460&v=4" alt="FotoPerfil"/>
-            <div className="user-info">
-              <strong>Carlos Oliveira</strong>
-              <span>ReactJS, React Native, Node.JS</span>
-            </div>
-          </header>
-          <p>Aqui tem uma historia muito linda, pode ter certeza! Combinado?</p>
-          <a href="https://github.com/burn-c">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars0.githubusercontent.com/u/54965836?s=460&v=4" alt="FotoPerfil"/>
-            <div className="user-info">
-              <strong>Carlos Oliveira</strong>
-              <span>ReactJS, React Native, Node.JS</span>
-            </div>
-          </header>
-          <p>Aqui tem uma historia muito linda, pode ter certeza! Combinado?</p>
-          <a href="https://github.com/burn-c">Acessar perfil no Github</a>
-        </li>
+        {devs.map(dev => (
+          <DevItem key={dev._id} dev={dev} />
+        ))}
+        
+
+        
       </ul>
     </main>
   </div>
